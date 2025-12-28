f995118dd967f8d781af31c37ffb12c324bd1f7f
Austin Riba <austin@m51.io>
2025-12-27 21:17:37 -0800

    Thread per connection

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,34 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("accepted new connection");
-                loop {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(n) => {
-                            if n == 0 {
+                println!("accepted new connection, spawning thread");
+                std::thread::spawn(move || {
+                    loop {
+                        let mut buffer = [0; 1024];
+                        match stream.read(&mut buffer) {
+                            Ok(n) => {
+                                if n == 0 {
+                                    break;
+                                }
+                                let input_str = String::from_utf8_lossy(&buffer[..n]);
+                                let input_str = input_str.trim();
+                                println!("received input: {}", input_str);
+                                let commands: Vec<_> =
+                                    input_str.split_terminator("\\r\\n").collect();
+                                commands
+                                    .iter()
+                                    .filter(|cmd| cmd.contains("PING"))
+                                    .for_each(|_| {
+                                        write!(stream, "+PONG\r\n").unwrap();
+                                    });
+                            }
+                            Err(e) => {
+                                println!("error: {}", e);
                                 break;
                             }
-                            let input_str = String::from_utf8_lossy(&buffer[..n]);
-                            let input_str = input_str.trim();
-                            println!("received input: {}", input_str);
-                            let commands: Vec<_> = input_str.split_terminator("\\r\\n").collect();
-                            commands
-                                .iter()
-                                .filter(|cmd| cmd.contains("PING"))
-                                .for_each(|_| {
-                                    write!(stream, "+PONG\r\n").unwrap();
-                                });
-                        }
-                        Err(e) => {
-                            println!("error: {}", e);
                         }
                     }
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);

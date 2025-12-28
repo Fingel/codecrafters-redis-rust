7ea9da64c8675f4d386423050525bc78b37f30ba
Austin Riba <austin@m51.io>
2025-12-27 21:07:20 -0800

    First multi ping attempt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -6,14 +6,36 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment the code below to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                write!(stream, "+PONG\r\n").unwrap();
                 println!("accepted new connection");
+                loop {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(n) => {
+                            if n == 0 {
+                                break;
+                            }
+                            let input_str = String::from_utf8_lossy(&buffer[..n]);
+                            let input_str = input_str.trim();
+                            println!("received input: {}", input_str);
+                            let commands: Vec<_> = input_str.split_terminator("\\r\\n").collect();
+                            commands
+                                .iter()
+                                .filter(|cmd| cmd.contains("PING"))
+                                .for_each(|_| {
+                                    write!(stream, "+PONG\r\n").unwrap();
+                                });
+                            break;
+                        }
+                        Err(e) => {
+                            println!("error: {}", e);
+                        }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);

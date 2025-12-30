62ea3f6d16d8ff9bb310cc9bf19f801d171945f2
Austin Riba <austin@m51.io>
2025-12-30 12:21:06 -0800

    Refactor argument parsing, error handling

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,7 +1,7 @@
 use bytes::Bytes;
+use thiserror::Error;
 
 use crate::parser::RedisValueRef;
-use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum RedisCommand {
@@ -14,33 +14,49 @@ pub enum RedisCommand {
     Lrange(Bytes, i64, i64),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum CmdError {
+    #[error("commands must start with a string")]
     InvalidCommandType,
+    #[error("unknown command '{0}'")]
     InvalidCommand(String),
+    #[error("invalid argument '{0}'")]
     InvalidArgument(String),
+    #[error("wrong number of arguments")]
     InvalidArgumentNum,
+    #[error("{field} must be a string")]
+    InvalidStringArg { field: String },
+    #[error("{field} must be an integer")]
+    InvalidIntegerArg { field: String },
+    #[error("could not parse {field} as integer")]
+    ParseError { field: String },
 }
 
-impl std::error::Error for CmdError {}
+fn extract_string_arg(arg: &RedisValueRef, field_name: &str) -> Result<Bytes, CmdError> {
+    arg.clone()
+        .as_string()
+        .map_err(|_| CmdError::InvalidStringArg {
+            field: field_name.to_string(),
+        })
+}
 
-impl fmt::Display for CmdError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            CmdError::InvalidCommandType => {
-                write!(f, "commands must start with a string")
-            }
-            CmdError::InvalidCommand(cmd) => {
-                write!(f, "unknown command '{}'", cmd)
-            }
-            CmdError::InvalidArgument(arg) => {
-                write!(f, "invalid argument: {}", arg)
-            }
-            CmdError::InvalidArgumentNum => {
-                write!(f, "wrong number of arguments")
-            }
-        }
-    }
+fn extract_lossy_string_arg(arg: &RedisValueRef, field_name: &str) -> Result<String, CmdError> {
+    let bytes_val = extract_string_arg(arg, field_name)?;
+    Ok(String::from_utf8_lossy(&bytes_val).to_string())
+}
+
+fn extract_integer_arg(arg: &RedisValueRef, field_name: &str) -> Result<i64, CmdError> {
+    let string_val = extract_lossy_string_arg(arg, field_name)?;
+    string_val.parse::<i64>().map_err(|_| CmdError::ParseError {
+        field: field_name.to_string(),
+    })
+}
+
+fn extract_u64_arg(arg: &RedisValueRef, field_name: &str) -> Result<u64, CmdError> {
+    let string_val = extract_lossy_string_arg(arg, field_name)?;
+    string_val.parse::<u64>().map_err(|_| CmdError::ParseError {
+        field: field_name.to_string(),
+    })
 }
 
 #[derive(Default)]
@@ -97,29 +113,13 @@ impl RedisInterpreter {
         if args.len() < 3 {
             return Err(CmdError::InvalidArgumentNum);
         }
-        let key = args[1]
-            .clone()
-            .as_string()
-            .map_err(|_| CmdError::InvalidArgument("key must be a string".into()))?
-            .clone();
-        let value = args[2]
-            .clone()
-            .as_string()
-            .map_err(|_| CmdError::InvalidArgument("value must be a string".into()))?;
+        let key = extract_string_arg(&args[1], "key")?;
+        let value = extract_string_arg(&args[2], "value")?;
         match args.len() {
             3 => Ok(RedisCommand::Set(key, value)),
             5 => {
-                let ttl_string = args[4]
-                    .clone()
-                    .as_lossy_string()
-                    .map_err(|_| CmdError::InvalidArgument("ttl value must be a string".into()))?;
-                let ttl_arg = ttl_string
-                    .parse::<u64>()
-                    .map_err(|_| CmdError::InvalidArgument("Could not parse ttl value".into()))?;
-                let ttl_type = args[3]
-                    .clone()
-                    .as_lossy_string()
-                    .map_err(|_| CmdError::InvalidArgument("ttl type must be a string".into()))?;
+                let ttl_type = extract_lossy_string_arg(&args[3], "ttl type")?;
+                let ttl_arg = extract_u64_arg(&args[4], "ttl value")?;
                 let ttl_val = match ttl_type.as_str() {
                     "EX" => ttl_arg * 1000,
                     "PX" => ttl_arg,
@@ -135,11 +135,7 @@ impl RedisInterpreter {
         if args.len() < 2 {
             Err(CmdError::InvalidArgumentNum)
         } else {
-            let key = args[1]
-                .clone()
-                .as_string()
-                .map_err(|_| CmdError::InvalidArgument("key must be a string".into()))?
-                .clone();
+            let key = extract_string_arg(&args[1], "key")?;
             Ok(RedisCommand::Get(key))
         }
     }
@@ -148,18 +144,11 @@ impl RedisInterpreter {
         if args.len() < 3 {
             Err(CmdError::InvalidArgumentNum)
         } else {
-            let key = args[1]
-                .clone()
-                .as_string()
-                .map_err(|_| CmdError::InvalidArgument("key must be a string".into()))?
-                .clone();
+            let key = extract_string_arg(&args[1], "key")?;
             let values: Result<Vec<Bytes>, CmdError> = args[2..]
                 .iter()
-                .map(|arg| {
-                    arg.clone()
-                        .as_string()
-                        .map_err(|_| CmdError::InvalidArgument("value must be a string".into()))
-                })
+                .enumerate()
+                .map(|(i, arg)| extract_string_arg(arg, &format!("value[{}]", i)))
                 .collect();
             let values = values?;
             Ok(RedisCommand::Rpush(key, values))
@@ -170,25 +159,9 @@ impl RedisInterpreter {
         if args.len() != 4 {
             Err(CmdError::InvalidArgumentNum)
         } else {
-            let key = args[1]
-                .clone()
-                .as_string()
-                .map_err(|_| CmdError::InvalidArgument("key must be a string".into()))?
-                .clone();
-            let start = args[2]
-                .clone()
-                .as_lossy_string()
-                .map_err(|_| CmdError::InvalidArgument("start must be an integer".into()))?;
-            let stop = args[3]
-                .clone()
-                .as_lossy_string()
-                .map_err(|_| CmdError::InvalidArgument("stop must be an integer".into()))?;
-            let start = start
-                .parse::<i64>()
-                .map_err(|_| CmdError::InvalidArgument("start must be an integer".into()))?;
-            let stop = stop
-                .parse::<i64>()
-                .map_err(|_| CmdError::InvalidArgument("stop must be an integer".into()))?;
+            let key = extract_string_arg(&args[1], "key")?;
+            let start = extract_integer_arg(&args[2], "start")?;
+            let stop = extract_integer_arg(&args[3], "stop")?;
             Ok(RedisCommand::Lrange(key, start, stop))
         }
     }

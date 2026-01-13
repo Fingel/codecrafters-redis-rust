a8c11ff4c7bfb6f56ee032a059a66d5d41c56336
Austin Riba <austin@m51.io>
2026-01-12 22:04:26 -0800

    Use StreamMap from tokio-stream to handle mutliple recievers concurrently

--- src/pubsub.rs
+++ src/pubsub.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
-
 use futures::{SinkExt, StreamExt};
 use tokio::net::TcpStream;
-use tokio::sync::broadcast::Receiver;
+use tokio_stream::StreamMap;
+use tokio_stream::wrappers::BroadcastStream;
 use tokio_util::codec::Framed;
 
 use crate::{
@@ -11,51 +10,71 @@ use crate::{
     parser::{RArray, RError, RInt, RString, RedisValueRef, RespParser},
 };
 
-type Subscriptions = HashMap<String, Receiver<RedisValueRef>>;
+type Subscriptions = StreamMap<String, BroadcastStream<RedisValueRef>>;
 
 pub async fn subscription_loop(
     db: &Db,
     transport: &mut Framed<TcpStream, RespParser>,
     channel: String,
 ) {
-    let mut subscriptions: Subscriptions = HashMap::new();
+    let mut subscriptions: Subscriptions = StreamMap::new();
     let resp = subscribe(db, channel, &mut subscriptions).await;
     transport.send(resp).await.unwrap();
+
     loop {
-        let result = transport.next().await.unwrap();
-        match result {
-            Ok(value) => {
-                let command: Result<RedisCommand, _> = value.try_into();
-                let resp = match command {
-                    Ok(RedisCommand::Subscribe(channel)) => {
-                        subscribe(db, channel, &mut subscriptions).await
+        tokio::select! {
+            Some((channel, result)) = subscriptions.next() => {
+                match result {
+                    Ok(message) => {
+                        transport.send(RArray(vec![
+                            RString("message"),
+                            RString(channel),
+                            message
+                        ])).await.unwrap();
                     }
-                    Ok(RedisCommand::Unsubscribe(channel)) => {
-                        unsubscribe(db, channel, &mut subscriptions).await
+                    Err(err) => {
+                        transport.send(RString(format!("Error: {}", err))).await.unwrap();
                     }
-                    Ok(RedisCommand::PSubscribe(pattern)) => {
-                        punsubscribe(db, pattern, &mut subscriptions).await
+                }
+            }
+            // gather all subscriptions and wait for the next message from any of them
+            Some(result) = transport.next() => {
+                match result {
+                    Ok(value) => {
+                        let command: Result<RedisCommand, _> = value.try_into();
+                        let resp = match command {
+                            Ok(RedisCommand::Subscribe(channel)) => {
+                                subscribe(db, channel, &mut subscriptions).await
+                            }
+                            Ok(RedisCommand::Unsubscribe(channel)) => {
+                                unsubscribe(db, channel, &mut subscriptions).await
+                            }
+                            Ok(RedisCommand::PSubscribe(pattern)) => {
+                                punsubscribe(db, pattern, &mut subscriptions).await
+                            }
+                            Ok(RedisCommand::PUnsubscribe(pattern)) => {
+                                punsubscribe(db, pattern, &mut subscriptions).await
+                            }
+                            Ok(RedisCommand::Ping) => ping(),
+                            Ok(other_command) => RError(format!(
+                                "ERR Can't execute {} in subscribed mode",
+                                other_command
+                            )),
+                            Err(e) => RError(format!(
+                                "ERR Can't execute command in subscribe mode: {:?}",
+                                e
+                            )),
+                        };
+                        transport.send(resp).await.unwrap();
                     }
-                    Ok(RedisCommand::PUnsubscribe(pattern)) => {
-                        punsubscribe(db, pattern, &mut subscriptions).await
+                    Err(e) => {
+                        eprintln!("Error reading from transport: {:?}", e);
+                        break;
                     }
-                    Ok(RedisCommand::Ping) => ping(),
-                    Ok(other_command) => RError(format!(
-                        "ERR Can't execute {} in subscribed mode",
-                        other_command
-                    )),
-                    Err(e) => RError(format!(
-                        "ERR Can't execute command in subscribe mode: {:?}",
-                        e
-                    )),
                 };
-                transport.send(resp).await.unwrap();
+
             }
-            Err(e) => {
-                eprintln!("Error reading from transport: {:?}", e);
-                break;
-            }
-        };
+        }
     }
 }
 
@@ -64,21 +83,32 @@ pub async fn subscribe(
     channel: String,
     subscriptions: &mut Subscriptions,
 ) -> RedisValueRef {
-    let sub_cnt = {
+    if subscriptions.contains_key(&channel) {
+        return RArray(vec![
+            RString("subscribe"),
+            RString(&channel),
+            RInt(subscriptions.len() as i64),
+        ]);
+    }
+
+    // TODO: make a db.subscribe(channel) method
+    let receiver = {
         let mut pubsub = db.pubsub.lock().unwrap();
         if let Some(tx) = pubsub.get(&channel) {
-            subscriptions.insert(channel.clone(), tx.subscribe());
+            tx.subscribe()
         } else {
             let (tx, rx) = tokio::sync::broadcast::channel::<RedisValueRef>(1024);
             pubsub.insert(channel.clone(), tx);
-            subscriptions.insert(channel.clone(), rx);
+            rx
         }
-        subscriptions.len()
     };
+    let stream = BroadcastStream::new(receiver);
+    subscriptions.insert(channel.clone(), stream);
+
     RArray(vec![
         RString("subscribe"),
         RString(channel),
-        RInt(sub_cnt as i64),
+        RInt(subscriptions.len() as i64),
     ])
 }
 
@@ -110,13 +140,13 @@ pub fn ping() -> RedisValueRef {
     RArray(vec![RString("pong"), RString("")])
 }
 
-pub async fn publish(db: &Db, channel: String, _message: String) -> RedisValueRef {
-    let cnt = db
-        .pubsub
-        .lock()
-        .unwrap()
-        .get(&channel)
-        .unwrap()
-        .receiver_count();
-    RInt(cnt as i64)
+pub async fn publish(db: &Db, channel: String, message: String) -> RedisValueRef {
+    let guard = db.pubsub.lock().unwrap();
+    if let Some(sender) = guard.get(&channel) {
+        let _ = sender.send(RString(message));
+        let cnt = sender.receiver_count() as i64;
+        RInt(cnt)
+    } else {
+        RInt(0)
+    }
 }

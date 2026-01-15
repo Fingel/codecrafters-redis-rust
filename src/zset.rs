75dee19ab25784f6cd281f7c606da7b1fe5312e9
Austin Riba <austin@m51.io>
2026-01-14 16:16:37 -0800

    Use a skiplist instead of skipmap to store keys with same score

--- src/zset.rs
+++ src/zset.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use ordered_float::NotNan;
-use skiplist::SkipMap;
+use skiplist::OrderedSkipList;
 
 use crate::{
     Db,
@@ -10,37 +10,72 @@ use crate::{
 
 type Score = NotNan<f64>;
 
+#[derive(Debug, PartialEq)]
+struct ListNode {
+    score: Score,
+    key: String,
+}
+
+impl ListNode {
+    fn new(score: f64, key: String) -> Self {
+        let score = Score::new(score).unwrap();
+        ListNode { score, key }
+    }
+}
+
+impl PartialOrd for ListNode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.score.partial_cmp(&other.score)
+    }
+}
+
 #[derive(Debug)]
 pub struct ZSet {
     map: HashMap<String, f64>,
-    list: SkipMap<Score, String>,
+    list: OrderedSkipList<ListNode>,
 }
 
 impl ZSet {
     fn new() -> Self {
         ZSet {
             map: HashMap::new(),
-            list: SkipMap::new(),
+            list: OrderedSkipList::new(),
         }
     }
 
-    fn add(&mut self, member: String, score: f64) {
-        self.map.insert(member.clone(), score);
-        self.list.insert(Score::new(score).unwrap(), member);
+    /// Add a member to the zset, returning the number of elements added
+    fn add(&mut self, member: String, score: f64) -> usize {
+        match self.map.get_mut(&member) {
+            Some(existing) => {
+                // find and remove item from the skiplist
+                let old_member = ListNode::new(*existing, member.clone());
+                self.list.remove(&old_member);
+                // update the score in the map
+                *existing = score;
+                // insert a new value into the skiplist
+                let new_member = ListNode::new(score, member);
+                self.list.insert(new_member);
+                0
+            }
+            None => {
+                self.map.insert(member.clone(), score);
+                self.list.insert(ListNode::new(score, member));
+                1
+            }
+        }
     }
 }
 
 pub async fn zadd(db: &Db, set: String, score: f64, member: String) -> RedisValueRef {
     let mut set_guard = db.zsets.lock().unwrap();
-    match set_guard.get_mut(&set) {
-        Some(zset) => {
-            zset.add(member, score);
-        }
+    let cnt = match set_guard.get_mut(&set) {
+        Some(zset) => zset.add(member, score),
         None => {
             let mut zset = ZSet::new();
-            zset.add(member, score);
-            set_guard.insert(set.clone(), zset);
+            let cnt = zset.add(member, score);
+            set_guard.insert(set, zset);
+            cnt
         }
-    }
-    RInt(1)
+    };
+    RInt(cnt as i64)
 }

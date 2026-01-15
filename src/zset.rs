fef067b07774e806c7b1a8b182c348b3089a4934
Austin Riba <austin@m51.io>
2026-01-14 16:38:26 -0800

    Convert ListNode to tuple struct, add tests

--- src/zset.rs
+++ src/zset.rs
@@ -10,28 +10,18 @@ use crate::{
 
 type Score = NotNan<f64>;
 
-#[derive(Debug, PartialEq)]
-struct ListNode {
-    score: Score,
-    key: String,
-}
-
-impl ListNode {
-    fn new(score: f64, key: String) -> Self {
-        let score = Score::new(score).unwrap();
-        ListNode { score, key }
-    }
-}
+#[derive(Debug, PartialEq, Clone)]
+struct ListNode(Score, String);
 
 impl PartialOrd for ListNode {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.score.partial_cmp(&other.score)
+        self.0.partial_cmp(&other.0)
     }
 }
 
 #[derive(Debug)]
 pub struct ZSet {
-    map: HashMap<String, f64>,
+    map: HashMap<String, Score>,
     list: OrderedSkipList<ListNode>,
 }
 
@@ -45,21 +35,22 @@ impl ZSet {
 
     /// Add a member to the zset, returning the number of elements added
     fn add(&mut self, member: String, score: f64) -> usize {
+        let score = Score::new(score).unwrap();
         match self.map.get_mut(&member) {
             Some(existing) => {
                 // find and remove item from the skiplist
-                let old_member = ListNode::new(*existing, member.clone());
+                let old_member = ListNode(*existing, member.clone());
                 self.list.remove(&old_member);
                 // update the score in the map
                 *existing = score;
                 // insert a new value into the skiplist
-                let new_member = ListNode::new(score, member);
+                let new_member = ListNode(score, member);
                 self.list.insert(new_member);
                 0
             }
             None => {
                 self.map.insert(member.clone(), score);
-                self.list.insert(ListNode::new(score, member));
+                self.list.insert(ListNode(score, member));
                 1
             }
         }
@@ -78,4 +69,41 @@ pub async fn zadd(db: &Db, set: String, score: f64, member: String) -> RedisValu
         }
     };
     RInt(cnt as i64)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::RedisDb;
+
+    use super::*;
+
+    fn setup() -> Arc<RedisDb> {
+        Arc::new(RedisDb::new(None, "/tmp/redis-files", "dump.rdb"))
+    }
+
+    #[tokio::test]
+    async fn test_zadd() {
+        let db = setup();
+        let cnt = zadd(&db, "test_set".to_string(), 1.0, "member1".to_string()).await;
+        assert_eq!(cnt, RInt(1));
+        // Same
+        let cnt = zadd(&db, "test_set".to_string(), 1.0, "member1".to_string()).await;
+        assert_eq!(cnt, RInt(0));
+    }
+
+    #[tokio::test]
+    async fn test_zadd_update() {
+        let db = setup();
+        let _ = zadd(&db, "test_set".to_string(), 1.0, "member1".to_string()).await;
+        let node = db.zsets.lock().unwrap().get("test_set").unwrap().list[0].clone();
+        assert_eq!(node.0, 1.0);
+        assert_eq!(node.1, "member1");
+        let _ = zadd(&db, "test_set".to_string(), 2.0, "member1".to_string()).await;
+        let node = db.zsets.lock().unwrap().get("test_set").unwrap().list[0].clone();
+        assert_eq!(node.0, 2.0);
+        assert_eq!(node.1, "member1");
+        // Same
+    }
 }

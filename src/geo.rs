236dda5c6a26c046687a0b8bc572a859822e7ff6
Austin Riba <austin@m51.io>
2026-01-15 17:00:07 -0800

    validate lng/lat

--- src/geo.rs
+++ src/geo.rs
@@ -1,8 +1,21 @@
 use crate::{
     Db,
-    parser::{RInt, RedisValueRef},
+    parser::{RError, RInt, RedisValueRef},
 };
 
-pub fn geoadd(_db: &Db, _set: String, _lng: f64, _lat: f64, _member: String) -> RedisValueRef {
+fn validate_lat_lng(lng: f64, lat: f64) -> Result<(), String> {
+    if !(-180.0..=180.0).contains(&lng) {
+        Err(format!("Longitude value ({}) is invalid", lng))
+    } else if !(-85.05112878..=85.05112878).contains(&lat) {
+        Err(format!("Latitude value ({}) is invalid", lat))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn geoadd(_db: &Db, _set: String, lng: f64, lat: f64, _member: String) -> RedisValueRef {
+    if let Err(err) = validate_lat_lng(lng, lat) {
+        return RError(format!("ERR {}", err));
+    }
     RInt(1)
 }
